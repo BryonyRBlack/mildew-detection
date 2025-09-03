2fbc3a10b4a0d410f84fbdacf3924b5bfd300690
BryonyRBlack <bryonyblack44@gmail.com>
2025-09-03 17:11:59 +0000

    Add project hypothesis page, to be completed fully later

---
+++ app_pages/page_project_hypothesis.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+def page_project_hypothesis_body():
+    st.write("~~~ Project Hypothesis and Validation")
+    st.success(
+        f"Mildew infected cells tend to show white powder on the leaves"
+    )
