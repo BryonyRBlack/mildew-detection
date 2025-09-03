8f7799aa976f78b639ce0857f607b82219c94a2a
BryonyRBlack <bryonyblack44@gmail.com>
2025-09-03 17:13:56 +0000

    Add page summary, to be completed later

---
+++ app_pages/page_summary.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+def page_summary_body():
+    st.write("~~~ Quick Prokect Summary ~~~")
+    st.info(
+        f"To be completed"
+    )
+    st.write(
+        f"To be completed"
+    )
+    st.success(
+        f"To be completed"
+    )
