c1d0e61b2b7c436e2ec0a3d85dce7350108fa296
BryonyRBlack <bryonyblack44@gmail.com>
2025-09-08 20:39:19 +0000

    Add hypothesis to project hypothesis page

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -13,6 +13,20 @@ def page_project_hypothesis_body():
         f"This was shown by:\n"
         f"* The Image Montage showed that the infected leaves"
         f"had white spots on them.\n"
+    )
+    st.info(
+        f"Healthy cherry leaves have more green visable as proven by their"
+        f"average and variability images."
+    )
+    st.success(
+        f"This was shown by:\n"
         f"* The average and varibility images showed a difference between"
-        f"the healthy and infected leaves."
+        f"the healthy and infected leaves.")
+    st.info(
+        f"That using Machine Learning, costs can be saved on health checks")
+    st.success(
+        f"Though this would not be proven until taken into practice."
+        f"Whilst each tree would need leaves taken and photographed,"
+        f"there is the ability to upload multiple images at the same,"
+        f"which would save time. This cannot be fully validated at this point."
     )
